bddb4ba1e847adf092bff835283ba44c39786f6d
Caique Jhones <caiquejh@gmail.com>
2018-04-10 22:57:01 -0300

    Adicionando textos para resultados

--- src/features/content/estiloAprendizagem.vue
+++ src/features/content/estiloAprendizagem.vue
@@ -7,7 +7,7 @@ export default {
       definition: `Este questionário está desenhado para conhecer seu Estilo de Aprendizagem.<br>
         Este questionário é anônimo.<br>
         Neste questionário não existem respostas corretas ou incorretas.<br>
-        <span class="font-weight-bold">Autores: </span>Catalina M. Alonso e Daniela Melaré.<br>
+        <span class="font-weight-bold">Autores: </span>Catalina M. Alonso, Domingo J. Galego e Peter Honey<br>
         `,
       instructions: 'Assinale as afirmativas que têm significado com seu estilo de seu Estilo de aprendizagem.',
       selected: []
@@ -29,6 +29,14 @@ export default {
     pragmatico () {
       return solveEstiloAprendizagem(this.selected).pragmatico
     }
+  },
+  methods: {
+    onfocus () {
+      let imgs = document.getElementsByClassName('imgs')
+      for (const element of imgs) {
+        element.style.cursor = 'pointer'
+      }
+    }
   }
 }
 </script>
@@ -46,12 +54,15 @@ export default {
     <b-form-group>
       <b-form-checkbox-group stacked v-model="selected" :options="options"/>
     </b-form-group>
-    <b-button v-b-modal.resultado>Verificar</b-button>
+    <b-button v-b-modal.resultado variant="primary" class="verify">Verificar</b-button>
 
     <b-modal id="resultado" title="Resultado" ok-only=true>
       <div>
         <b-row>
-          <b-col cols="4" sm="2"><b-img src="../../assets/reflexivo.png" fluid/></b-col>
+          <b-col cols="4" sm="2"><b-img id="reflexivo" class="imgs" v-on:mouseover="onfocus()" src="../../assets/reflexivo.png" fluid/></b-col>
+          <b-popover target="reflexivo" title="Estilo Reflexivo" triggers="click blur"
+          content="Tem como elemento central como busca e pesquisa, no qual  o usuário aprende mediante a busca, seleção e organição do conteúdos. Os materiais de aprendizagem devem estar voltados a construções e sínteses que englobem a pesquisa  de um conteúdo. Portanto, sua denominação é estilo de uso busca e pesquisa  espaço virtual.">
+          </b-popover>
           <b-col cols="8" sm="10">
             <p class="head-text reflexivo">Estilo Reflexivo</p>
             <p class="info-percent">{{ reflexivo }}%</p>
@@ -60,7 +71,10 @@ export default {
       </div>
       <div>
         <b-row>
-          <b-col cols="4" sm="2"><b-img src="../../assets/ativo.png" fluid/></b-col>
+          <b-col cols="4" sm="2"><b-img id="ativo" class="imgs" v-on:mouseover="onfocus()" src="../../assets/ativo.png" fluid/></b-col>
+          <b-popover target="ativo" title="Estilo Ativo" triggers="click blur"
+          content="Este nível de uso considera a participação como elemento central, no qual o indivíduo deve ter a ambiência do espaço. Além disso, para realizar um processo de aprendizagem no virtual, o nível A nescessita de metodologias e materiais que priorizem o contato com grupos online, que solicite buscar informações online, realizar trabalhos em grupo, realizar fóruns de discussão e dar ações aos materiais desenvolvidos. Portando, sua denominação é estilo de uso participativo no espaço virtal.">
+          </b-popover>
           <b-col cols="8" sm="10">
             <p class="head-text ativo">Estilo Ativo</p>
             <p class="info-percent">{{ ativo }}%</p>
@@ -69,7 +83,10 @@ export default {
       </div>
       <div>
         <b-row>
-          <b-col cols="4" sm="2"><b-img src="../../assets/teorico.png" fluid/></b-col>
+          <b-col cols="4" sm="2"><b-img id="teorico" class="imgs" v-on:mouseover="onfocus()" src="../../assets/teorico.png" fluid/></b-col>
+          <b-popover target="teorico" title="Estilo Teórico" triggers="click blur"
+          content="Tem como elemento central para a aprendizagem a necessidade de desenvolver atividades que valorizem os aplicativos para gerar conteúdos e atividades de planejamento. Essas atividades devem basear-se em teorias e fundamentos sobre o que está desenvolvendo. Ficou denominado como estilo de estruturação e planejamento no espaço virtual">
+          </b-popover>
           <b-col cols="8" sm="10">
             <p class="head-text teorico">Estilo Teórico</p>
             <p class="info-percent">{{ teorico }}%</p>
@@ -78,7 +95,10 @@ export default {
       </div>
       <div>
         <b-row>
-          <b-col cols="4" sm="2"><b-img src="../../assets/pragmatico.png" fluid/></b-col>
+          <b-col cols="4" sm="2"><b-img id="pragmatico" class="imgs" v-on:mouseover="onfocus()" src="../../assets/pragmatico.png" fluid/></b-col>
+          <b-popover target="pragmatico" title="Estilo Pragmático" triggers="click blur"
+          content="Tem como elemento central para a aprendizagem a necessidade de realização de serviços online e a rapidez na realização desse processo. Viabilizar  com rapidez é um dos eixos centrais deste estilo de uso; utilizar o espaço virtual como um espaço de ação e produção. Foi denominado de estilo de ação concreta e produção no espaço virtual.">
+          </b-popover>
           <b-col cols="8" sm="10">
             <p class="head-text pragmatico">Estilo Pragmático</p>
             <p class="info-percent">{{ pragmatico }}%</p>
@@ -120,5 +140,8 @@ export default {
 }
 .info-percent {
   font-size: 1.8em;
+}
+.verify {
+  margin-bottom: 32px
 }
 </style>
